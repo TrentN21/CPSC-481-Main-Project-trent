aaf47953c029b32eae610c65f68f3c58340ca615
Dtran048 <152100454+Dtran048@users.noreply.github.com>
2026-04-01 13:06:30 -0700

    drew board

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
-#!/usr/bin/env python3
-
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((1600, 900))
+
+x = 800
+y = 400
+
+screen = pygame.display.set_mode((x, y))
+screen.fill('bisque2')
+
+goal = pygame.Surface((x/10,y/1.5))
+goal.fill('bisque1')
+
+spot = pygame.Surface((x/10,y/4))
+spot.fill('bisque1')
+ 
+
 clock = pygame.time.Clock()
 running = True
+
+
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill("white")
-
+    screen.blit(goal, (x/25,y/6))
+    screen.blit(goal, (x/1.15,y/6))
+    screen.blit(spot, (x/6.5,y/6))
+    screen.blit(spot, (x/3.7,y/6))
+    screen.blit(spot, (x/2.58,y/6))
+    screen.blit(spot, (x/1.98,y/6))
+    screen.blit(spot, (x/1.6,y/6))
+    screen.blit(spot, (x/1.3335,y/6))
+    screen.blit(spot, (x/6.5,y/1.7))
+    screen.blit(spot, (x/3.7,y/1.7))
+    screen.blit(spot, (x/2.58,y/1.7))
+    screen.blit(spot, (x/1.98,y/1.7))
+    screen.blit(spot, (x/1.6,y/1.7))
+    screen.blit(spot, (x/1.3335,y/1.7))
+    
+    
+    
     pygame.display.flip()
 
     clock.tick(60)
